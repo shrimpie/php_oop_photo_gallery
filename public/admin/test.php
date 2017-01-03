e471ceb764dc61b41d48194b51fbdca62ef0ce38
杨振宇 <andy@young.local>
2017-01-03 18:44:02 -0500

    Add user class and photograph class and upload file module.

---
+++ public/admin/test.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../../includes/initialize.php');
+if (!$session->is_logged_in()) { redirect_to("login.php"); }
+?>
+
+<?php include_layout_template('admin_header.php'); ?>
+
+	<?php
+		// $user = new User();
+		// $user->username = "johnsmith";
+		// $user->password = "abcd12345";
+		// $user->first_name = "John";
+		// $user->last_name = "Smith";
+		// $user->create();
+
+		// $user = User::find_by_id(2);
+		// $user->password = "12345wxyz";
+		// $user->save();
+
+		$user = User::find_by_id(2);
+		$user->delete();
+	?>
+
+<?php include_layout_template('admin_footer.php'); ?>
+		
